15ad034c4c8aec3945e7ebcb83fecfed3787ec96
Dhava2010 <87840766+Dhava2010@users.noreply.github.com>
2025-11-12 14:22:37 -0500

    Update testpi.py

--- testpi.py
+++ testpi.py
@@ -3,7 +3,6 @@ import cv2
 
 app = Flask(__name__)
 
-# Open USB camera (usually /dev/video0)
 camera = cv2.VideoCapture(0)
 
 def generate_frames():
@@ -12,10 +11,10 @@ def generate_frames():
         if not success:
             break
         else:
-            # Encode frame as JPEG
+
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            # Yield frame in HTTP multipart stream
+
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -29,5 +28,4 @@ def index():
     return '<h1>Raspberry Pi Camera Stream</h1><img src="/video" width="640" />'
 
 if __name__ == '__main__':
-    # Host on all interfaces (so you can access it via Pi's IP)
     app.run(host='0.0.0.0', port=8080, debug=False)
